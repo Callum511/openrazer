ab16eac2791be375602ceb2849b48fe0a04f0adf
Callum511 <callum.greer@gmail.com>
2021-12-26 19:23:02 +0000

    Updated keyboard matrix value for Blade 15 2020 (Early)

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -1337,7 +1337,7 @@ class RazerBladeEarly2020Base(_RippleKeyboard):
     USB_VID = 0x1532
     USB_PID = 0x0255
     HAS_MATRIX = True
-    MATRIX_DIMS = [1, 16]
+    MATRIX_DIMS = [6, 16]
     METHODS = ['get_device_type_keyboard', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
                'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect',
                'set_breath_dual_effect', 'set_custom_effect', 'set_key_row',
